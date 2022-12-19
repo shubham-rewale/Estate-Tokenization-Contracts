aff174ca218ccf4f3725da090d7225d47c598368
subhajitdas1999 <sd651846@gmail.com>
2022-12-19 18:53:15 +0530

    moved merkletree in the contructor

--- utils/markleTree.js
+++ utils/markleTree.js
@@ -4,16 +4,16 @@ const keccak256 = require("keccak256");
 class MogulDAOMarkleTree {
   constructor(propertyOwnerAddresses) {
     this.propertyOwnerAddresses = propertyOwnerAddresses;
-    this.propertyOwnersMarkleTree;
-  }
 
-  getOwnersRootHash() {
     const leafNodes = this.propertyOwnerAddresses.map((addr) =>
       keccak256(addr)
     );
     this.propertyOwnersMarkleTree = new MerkleTree(leafNodes, keccak256, {
       sortPairs: true,
     });
+  }
+
+  getOwnersRootHash() {
     const rootHash = this.propertyOwnersMarkleTree.getHexRoot();
     return rootHash;
   }
