ebc4dad0fbced6a3077563a6b9a6558b028b2f62
subhajitdas1999 <sd651846@gmail.com>
2022-12-19 17:50:45 +0530

    added mock withdraw funtion

--- contracts/MockWithDrawFunds.sol
+++ contracts/MockWithDrawFunds.sol
@@ -2,13 +2,27 @@
 pragma solidity ^0.8.17;
 
 contract MockWithDrawFunds {
-    function balance() public view returns (uint256 balance) {
+    function balanceOf() public view returns (uint256 balance) {
         return address(this).balance;
     }
 
+    event Received(address from);
+
     receive() external payable {}
 
-    function send(address _receiver) external {
-        payable(_receiver).transfer(address(this).balance);
+    // function send(address payable _receiver) external {
+    //     _receiver.transfer(address(this).balance);
+    //     emit Received(msg.sender);
+    // }
+
+    //  uint256 _tokenId,
+    //     uint256 _amountToWithdraw,
+
+    function withdrawFromMaintenanceReserve(
+        uint256 _tokenId,
+        uint256 _amountToWithdraw,
+        address payable _recepientAddr
+    ) public {
+        _recepientAddr.transfer(1000);
     }
 }
