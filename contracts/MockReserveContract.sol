08784a6d362943c742b653bae3b87fb5c7e2ed33
subhajitdas1999 <sd651846@gmail.com>
2022-12-20 16:13:37 +0530

    Make it UUPS compatible

--- contracts/MockReserveContract.sol
+++ contracts/MockReserveContract.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract MockReserveContract {
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract MockReserveContract is OwnableUpgradeable, UUPSUpgradeable {
+    function initialize() public initializer {
+        __Ownable_init();
+    }
+
     function balanceOf() public view returns (uint256 balance) {
         return address(this).balance;
     }
@@ -15,6 +22,9 @@ contract MockReserveContract {
         uint256 _amountToWithdraw,
         address payable _recepientAddr
     ) public {
-        _recepientAddr.transfer(1000);
+        _recepientAddr.transfer(_amountToWithdraw);
     }
+
+    // Need to add this function because of UUPS proxy
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 }
