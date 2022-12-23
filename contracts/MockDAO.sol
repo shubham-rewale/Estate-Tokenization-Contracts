9ee561a56b1e13ebb6cedb553b28ec589b9bde5b
subhajitdas1999 <sd651846@gmail.com>
2022-12-23 16:06:52 +0530

    Changed versions

--- contracts/MockDAO.sol
+++ contracts/MockDAO.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity >=0.8.9 <0.8.17;
+
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/TimersUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
