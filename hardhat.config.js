4c128a59defea1eb800b187adf12a8092ad7609d
subhajitdas1999 <sd651846@gmail.com>
2023-01-10 15:29:09 +0530

    Restore default network to hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,49 +2,58 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
-require('solidity-coverage')
+require("solidity-coverage");
 require("dotenv").config({ path: "./.env" });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-	solidity: {
-		compilers: [
-			{
-				version: "0.8.9",
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 200,
-					},
-				},
-			},
-			{
-				version: "0.8.17",
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 200,
-					},
-				},
-			},
-		],
-	},
-	defaultNetwork: "hardhat",
-	networks: {
-		hardhat: {
-			allowUnlimitedContractSize: true,
-		},
-		mumbai: {
-			url: process.env.MUMBAI_URL || "",
-			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-		},
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    // tmp: {
+    //   url: "http://127.0.0.1:8545/",
+    //   accounts: [
+    //     "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+    //     "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+    //   ],
+    // },
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    mumbai: {
+      url: process.env.MUMBAI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
 
-		goerli: {
-			url: process.env.GOERLI_URL || "",
-			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-		},
-	},
-	etherscan: {
-		apiKey: process.env.ETHERSCAN_API_KEY,
-	},
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
